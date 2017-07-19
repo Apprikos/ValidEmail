f8e047bedbcebef8d87558e775174242f007e0b8
Apprikos <ApprikosApplications@gmail.com>
2017-07-19 09:48:30 +0200

    Use Angular 2 email validation and only show message when user clicks away

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# ValidEmail
+# Valid Email?
 
-# Checks if email is valid client side 
+> Checks if email is valid client side 
+> Checks if user has made a typo and suggests alternative
+> 
--- src/app/emailform/emailform.component.ts
+++ src/app/emailform/emailform.component.ts
@@ -1,34 +1,56 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-emailform',
   templateUrl: './emailform.component.html',
   styleUrls: ['./emailform.component.scss']
 })
 export class EmailformComponent {
-	
+
 	user = {email:'',
-	password:''};
+	password:''} ;
 	
 	submitted = false;
 	typo = false;
 	isEmail = false;
-		
+	time = null;
+	wait = false;
+	message = false;
+	
 	onSubmit() {
 		this.submitted = true;
 	}
+	
+	validate(){
+		console.log("validate");
+		this.message = true;
+	}
+	
  
 	valuechange(newValue) {
-		this.isEmail = false;
+		this.message = false;
+		this.isEmail = true;
 		this.user.email = newValue;
+		console.log(newValue);
 		
+		
+		// check there is a . after @ (not done buy angular validator)
 		// find @
 		var at = newValue.indexOf("@");
 		// if there is an @
-		if(at > -1){
+		if(at > -1 && at != newValue.length-1){
+			this.isEmail = false;
 			console.log("contains @");
 			// see if there is a .
 			var after = newValue.substring(at, newValue.length);
+			
+			// prevent double message where angular does not allow end in .
+			if(after.indexOf(".") == after.length-1){
+				this.isEmail = true;
+			}
+			
+			// if . exists hide message
 			if(after.indexOf(".") > -1 && after.indexOf(".") != after.length-1){
 				console.log("contains .");
 				// is an email
@@ -36,8 +58,11 @@ export class EmailformComponent {
 			
 			}
 		}
+			
+		
 		
 		console.log("IsEmail "+this.isEmail);
+		
 	}
 
 }
